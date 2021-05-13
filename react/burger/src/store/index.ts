1438f73402b7fb72bfbd6d12ff0e5a16f504b6dd
troexol <vanyavanya0707@mail.ru>
2021-05-13 16:07:12 +0500

    refactor: 💡 redux store: +throttle

--- react/burger/src/store/index.ts
+++ react/burger/src/store/index.ts
@@ -1,10 +1,12 @@
 // *** NPM ***
 import { createStore, compose, combineReducers } from 'redux';
+import { throttle } from 'lodash';
 
 import { loadFromLocalStorage, saveToLocalStorage } from '../utils/local-storage';
 import ingredientsReducer from './ingredients';
 
 const LOCAL_STORAGE_REDUX_NAME = 'REDUX_STATE';
+const LOCAL_STORAGE_THROTTLE_TIME = 1000;
 
 // *** REDUX STORE ***
 const composeEnhancers =
@@ -22,9 +24,11 @@ const store = createStore(
     composeEnhancers(),
 );
 
-store.subscribe(() => {
-    saveToLocalStorage(store.getState(), LOCAL_STORAGE_REDUX_NAME);
-});
+store.subscribe(
+    throttle(() => {
+        saveToLocalStorage(store.getState(), LOCAL_STORAGE_REDUX_NAME);
+    }, LOCAL_STORAGE_THROTTLE_TIME),
+);
 
 export type StoreType = ReturnType<typeof rootReducer>;
 export type StoreDispatchType = typeof store.dispatch;
